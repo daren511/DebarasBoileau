01ed80b41c4508d8008e6d9adc409afc1e278d47
hcfranck <hc_throwdown@hotmail.com>
2014-05-20 08:59:05 -0400

    Somes changes about session , still need work

--- src/java/Catalogue/Catalogue.java
+++ src/java/Catalogue/Catalogue.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package Catalogue;
 
@@ -49,9 +49,7 @@ public class Catalogue extends HttpServlet {
          out.println("<div class=\"connexion\">");
          out.println("<form action=\"ConnexionUser\" method=\"POST\">");
          out.println("<table>");
-         out.println("<tr><td> Nom d'usager : </td><td> <input id=\"Username\" type=\"text\" class=\"Text_Box\" name=\"User\" /> </td></tr>");
-         out.println("<tr><td> Mot de passe : </td><td> <input id=\"Password\" type=\"password\" class=\"Text_Box\" name=\"Password\" /> </td></tr>");
-         out.println("<tr><td><button id=\"btnconnexion\" type=\"submit\" class=\"BTN_Connexion\">Se connecter</button></td></tr>");
+         ecrireConn(out, session, request);
          out.println("</table>");
          out.println("</br>");
          out.println("</form>");
@@ -75,6 +73,7 @@ public class Catalogue extends HttpServlet {
          out.println("<th>Genre</th>");
          out.println("<th>Prix</th>");
          out.println("<th>Quantité disponible</th>");
+         out.println("<th></th>");
          out.println("</tr>");
          listerTous(out);
          out.println("</table>");
@@ -98,9 +97,7 @@ public class Catalogue extends HttpServlet {
          out.println("<div class=\"connexion\">");
          out.println("<form action=\"ConnexionUser\" method=\"POST\">");
          out.println("<table>");
-         out.println("<tr><td> Nom d'usager : </td><td> <input id=\"Username\" type=\"text\" class=\"Text_Box\" name=\"User\" /> </td></tr>");
-         out.println("<tr><td> Mot de passe : </td><td> <input id=\"Password\" type=\"password\" class=\"Text_Box\" name=\"Password\" /> </td></tr>");
-         out.println("<tr><td><button id=\"btnconnexion\" type=\"submit\" class=\"BTN_Connexion\">Se connecter</button></td></tr>");
+         ecrireConn(out, session, request);
          out.println("</table>");
          out.println("</br>");
          out.println("</form>");
@@ -124,6 +121,7 @@ public class Catalogue extends HttpServlet {
          out.println("<th>Genre</th>");
          out.println("<th>Prix</th>");
          out.println("<th>Quantité disponible</th>");
+         out.println("<th></th>");
          out.println("</tr>");
          listerParGenre(genre, out);
          out.println("</table>");
@@ -135,64 +133,79 @@ public class Catalogue extends HttpServlet {
    }
    
    private void ecrireTete(PrintWriter writer, String Titre){
-        writer.println("<head>");
-        writer.println("<meta charset=\"utf-8\" />");
-        writer.println("<title>"+Titre+"</title>");
-        writer.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"WebApp.css\">");
-        writer.println("</head>");
-    }
-   protected void listerTous(PrintWriter out){
-        
-      try
-        {
-           //Connexion DB
-           ConnectionOracle oradb = new ConnectionOracle();
-           oradb.connecter();
-           // Déclaration
-           String sqltous = "select * from items";
-           ResultSet rstTous;
-           // Lister les items
-            Statement stm1 = oradb.getConnexion().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rstTous = stm1.executeQuery(sqltous);
-            
-            while (rstTous.next())
-            {
-               out.println("<tr><td>"+rstTous.getString(2).toString()+"</td>"+"<td>"+rstTous.getString(3).toString()+"</td>"+"<td>"+rstTous.getString(4).toString()+"</td>"+"<td>"
-                       + rstTous.getString(5).toString()+"</td>");
-               out.println("<td><button id=\"btnajouter\" type=\"submit\" class=\"BTN_Ajouter\">Ajouter Au Panier</button></td>");
-               out.println("</tr>");
-            
-            }
-        }
-        catch(SQLException sqlex){ System.out.println(sqlex);}
-         
-         
-         
+      writer.println("<head>");
+      writer.println("<meta charset=\"utf-8\" />");
+      writer.println("<title>"+Titre+"</title>");
+      writer.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"WebApp.css\">");
+      writer.println("</head>");
+   }
+   private void ecrireConn(PrintWriter out , HttpSession session , HttpServletRequest request) {
+      session = request.getSession();
+      if( session == null)
+      {
+         out.println("<tr><td> Nom d'usager : </td><td> <input id=\"Username\" type=\"text\" class=\"Text_Box\" name=\"User\" /> </td></tr>");
+         out.println("<tr><td> Mot de passe : </td><td> <input id=\"Password\" type=\"password\" class=\"Text_Box\" name=\"Password\" /> </td></tr>");
+         out.println("<tr><td><button id=\"btnconnexion\" type=\"submit\" class=\"BTN_Connexion\">Se connecter</button></td></tr>");
       }
+      else
+      {
+         out.println("<tr><td> Bienvenue à vous "+session.getAttribute("User"));
+         out.println(session.getAttribute("Ecus"));
+      }
+      
+   }
+   protected void listerTous(PrintWriter out){
+      
+      try
+      {
+         //Connexion DB
+         ConnectionOracle oradb = new ConnectionOracle();
+         oradb.connecter();
+         // Déclaration
+         String sqltous = "select * from items";
+         ResultSet rstTous;
+         // Lister les items
+         Statement stm1 = oradb.getConnexion().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+         rstTous = stm1.executeQuery(sqltous);
+         
+         while (rstTous.next())
+         {
+            out.println("<tr><td>"+rstTous.getString(2).toString()+"</td>"+"<td>"+rstTous.getString(3).toString()+"</td>"+"<td>"+rstTous.getString(4).toString()+"</td>"+"<td>"
+                    + rstTous.getString(5).toString()+"</td>");
+            out.println("<td><button id=\"btnajouter\" type=\"submit\" class=\"BTN_Ajouter\">Ajouter Au Panier</button></td>");
+            out.println("</tr>");
+            
+         }
+      }
+      catch(SQLException sqlex){ System.out.println(sqlex);}
+      
+      
+      
+   }
    protected void listerParGenre(String genre ,PrintWriter out){
       try
-        {
-           //Connexion DB
-           ConnectionOracle oradb = new ConnectionOracle();
-           oradb.connecter();
-           
-            CallableStatement stm1 = oradb.getConnexion().prepareCall("{? = call GESTIONINVENTAIRE.LISTER(?)}",ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            stm1.registerOutParameter(1, OracleTypes.CURSOR);
-            stm1.setString(2, genre);
-            stm1.execute();
-            ResultSet rstItems =(ResultSet)stm1.getObject(1);
-            while (rstItems.next())
-            {
-               out.println("<tr><td>"+rstItems.getString(2).toString()+"</td>"+"<td>"+rstItems.getString(5).toString()+"</td>"+"<td>"+rstItems.getString(3).toString()+"</td>"+"<td>"
-                       + rstItems.getString(4).toString()+"</td>");
-               out.println("<td><button id=\"btnajouter\" type=\"submit\" class=\"BTN_Ajouter\">Ajouter Au Panier</button></td>");
-               out.println("</tr>");
-            }
-        }
-        catch(SQLException sqlex){ System.out.println(sqlex);}
-}
-  
-
+      {
+         //Connexion DB
+         ConnectionOracle oradb = new ConnectionOracle();
+         oradb.connecter();
+         
+         CallableStatement stm1 = oradb.getConnexion().prepareCall("{? = call GESTIONINVENTAIRE.LISTER(?)}",ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+         stm1.registerOutParameter(1, OracleTypes.CURSOR);
+         stm1.setString(2, genre);
+         stm1.execute();
+         ResultSet rstItems =(ResultSet)stm1.getObject(1);
+         while (rstItems.next())
+         {
+            out.println("<tr><td>"+rstItems.getString(2).toString()+"</td>"+"<td>"+rstItems.getString(5).toString()+"</td>"+"<td>"+rstItems.getString(3).toString()+"</td>"+"<td>"
+                    + rstItems.getString(4).toString()+"</td>");
+            out.println("<td><button id=\"btnajouter\" type=\"submit\" class=\"BTN_Ajouter\">Ajouter Au Panier</button></td>");
+            out.println("</tr>");
+         }
+      }
+      catch(SQLException sqlex){ System.out.println(sqlex);}
+   }
+   
+   
    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
    /**
     * Handles the HTTP <code>GET</code> method.
@@ -208,7 +221,7 @@ public class Catalogue extends HttpServlet {
       processRequestTous(request, response);
       System.out.println("GET");
    }
-
+   
    /**
     * Handles the HTTP <code>POST</code> method.
     *
@@ -220,30 +233,30 @@ public class Catalogue extends HttpServlet {
    @Override
    protected void doPost(HttpServletRequest request, HttpServletResponse response)
            throws ServletException, IOException {
-           // Recup du genre
-           String genre = request.getParameter("Genre");
-           //avertit le navigateur qu'il va recevoir du code HTML
-           response.setContentType("text/html;charset=UTF-8");
-        
-            //Flux ecriture
-            PrintWriter out = response.getWriter();
-           try
-           {
-            if(genre.equals("Tous"))
-           {
-              processRequestTous(request, response); 
-           }
-            else
-            {
-               processRequestGenre(request, response, genre);
-            }
-           }
-           finally
-           {
-              out.close();
-           }
+      // Recup du genre
+      String genre = request.getParameter("Genre");
+      //avertit le navigateur qu'il va recevoir du code HTML
+      response.setContentType("text/html;charset=UTF-8");
+      
+      //Flux ecriture
+      PrintWriter out = response.getWriter();
+      try
+      {
+         if(genre.equals("Tous"))
+         {
+            processRequestTous(request, response);
+         }
+         else
+         {
+            processRequestGenre(request, response, genre);
+         }
+      }
+      finally
+      {
+         out.close();
+      }
    }
-
+   
    /**
     * Returns a short description of the servlet.
     *
@@ -253,5 +266,5 @@ public class Catalogue extends HttpServlet {
    public String getServletInfo() {
       return "Short description";
    }// </editor-fold>
-
+   
 }
