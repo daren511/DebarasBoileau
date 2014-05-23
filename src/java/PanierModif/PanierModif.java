ac1ed77f163b4fa161a60175f3fb0f5f7ab150c8
Ekmort <ydmule@hotmail.com>
2014-05-23 16:26:35 -0400

    PanierModif(broke pour L'instant)

---
+++ src/java/PanierModif/PanierModif.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PanierModif;
+
+import ConnexionUser.ConnectionOracle;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author 200666155
+ */
+@WebServlet(name = "PanierModif", urlPatterns = {"/PanierModif"})
+public class PanierModif extends HttpServlet {
+    private HttpSession session;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        //try (PrintWriter out = response.getWriter()) {
+        session = request.getSession();
+        int idItem = Integer.parseInt(request.getParameter("Item"));
+        String user = (String)session.getAttribute("User");
+        Modifier(idItem,user);
+        
+        response.sendRedirect("http://localhost:8084/DebarasBoileau/Panier");
+    }
+    
+    private void Modifier(int idItem,String user){
+        try
+        {
+            ConnectionOracle oradb = new ConnectionOracle();
+            oradb.connecter();
+            
+            CallableStatement stm1=oradb.getConnexion().prepareCall("{call GESTIONPANIER.ModifierPanier(?,?)}");
+            
+            stm1.setString(1,user);
+            stm1.setInt(2, idItem);
+            
+            stm1.executeUpdate(); 
+        }
+        catch(SQLException sqlex){ System.out.println(sqlex);} 
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
